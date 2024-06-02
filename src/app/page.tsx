fdf81f4de419c8675eef5ae316341bac0fcfd1c7
River-ans <mriveroom@gmail.com>
2024-06-02 19:56:29 +0900

    chore: 초기 프로젝트 설정 및 의존성 설치
    - 초기 프로젝트 구조 추가
    - 상태 관리를 위한 Zustand 설치
    - Bun 패키지 매니저 설정
    - 빈 디렉토리를 위한 .gitkeep 파일 포함

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,24 @@
-import Image from 'next/image';
+'use client';
+import { useState } from 'react';
 
 export default function Home() {
-  return <main>123</main>;
+  const [query, setQuery] = useState('');
+
+  const handleKeyDown = (e: { key: string }) => {
+    if (e.key === 'Enter') {
+      window.open(`https://www.google.com/search?q=${query}`, '_blank');
+    }
+  };
+
+  return (
+    <main className='flex flex-col'>
+      RIVER WORK DASHBORD
+      <input
+        type='text'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+    </main>
+  );
 }
